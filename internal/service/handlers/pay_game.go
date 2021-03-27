3dcb6ed075eabe1430d848f93f1f82fbf6adff30
Kirill Chernov <kirillchernov@gmail.com>
2021-03-27 23:31:25 +0200

    2.0upd

--- internal/service/handlers/pay_game.go
+++ internal/service/handlers/pay_game.go
@@ -1,7 +1,65 @@
 package handlers
 
-import "net/http"
+import (
+	"github.com/olegfomenko/game-service/internal/service/requests"
+	"gitlab.com/distributed_lab/ape"
+	"gitlab.com/distributed_lab/ape/problems"
+	"gitlab.com/tokend/go/xdrbuild"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 func PayGame(w http.ResponseWriter, r *http.Request) {
-	// TODO op1: Pay Gam admin -> user, op2: Pay usd user -> admin
+	request, err := requests.NewPayGame(r)
+	if err != nil {
+		Log(r).WithError(err).Error("invalid request")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
+
+	assetCode := request.Data.Attributes.GameCoinId
+	amount := uint64(request.Data.Attributes.Amount)
+
+	//TODO maybe use details as comment with donate
+	issueGam := &xdrbuild.CreateIssuanceRequest{
+		Reference: strconv.Itoa(time.Now().Nanosecond()),
+		Receiver:  request.Data.Attributes.OwnerId,
+		Asset:     assetCode,
+		Amount:    amount,
+		Details:   nil,
+		AllTasks:  nil,
+	}
+
+	orgBalance, err := Connector(r).GetBalanceByID(request.Data.Attributes.SourceBalanceId)
+	if err != nil {
+		Log(r).WithError(err).Error("error getting organizer balance")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
+
+	// TODo fix reference
+	paymentToAdmin, _ := xdrbuild.CreatePaymentForAccount(xdrbuild.CreatePaymentForAccountOpts{
+		SourceAccountID:      &orgBalance.Relationships.Owner.Data.ID,
+		SourceBalanceID:      orgBalance.ID,
+		DestinationAccountID: Connector(r).Signer().Address(),
+		Amount:               amount,
+		Subject:              "Game payment",
+		Reference:            strconv.Itoa(time.Now().Nanosecond()),
+		Fee: xdrbuild.Fee{
+			SourceFixed:        0,
+			SourcePercent:      0,
+			DestinationFixed:   0,
+			DestinationPercent: 0,
+			SourcePaysForDest:  false,
+		},
+	})
+
+	_, err = Connector(r).TxSigned(Connector(r).Signer(), issueGam, paymentToAdmin)
+	if err != nil {
+		Log(r).WithError(err).Error("error sending transaction")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
+
 }
