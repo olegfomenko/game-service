f4d99c05bbe3126180e82fe9b23dc2ae7733da2b
olegfomenko <oleg.fomenko2002@gmail.com>
2021-03-27 20:11:26 +0200

    fix create game schema

--- resources/model_team.go
+++ resources/model_team.go
@@ -5,10 +5,6 @@
 package resources
 
 type Team struct {
-	Acc1     string `json:"acc1"`
-	Acc2     string `json:"acc2"`
-	Acc3     string `json:"acc3"`
-	Acc4     string `json:"acc4"`
-	Acc5     string `json:"acc5"`
-	TeamName string `json:"team_name"`
+	Players  *map[string]interface{} `json:"players,omitempty"`
+	TeamName string                  `json:"team_name"`
 }
