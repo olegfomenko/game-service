60bb10247285870c1910b1cd274b10f22753edbf
Kirill Chernov <kirillchernov@gmail.com>
2021-03-27 20:12:47 +0200

    all paths

--- internal/service/requests/create_game.go
+++ internal/service/requests/create_game.go
@@ -21,8 +21,6 @@ func NewCreateGame (r *http.Request) (resources.CreateGameResponse, error)  {
 func validateCreateGame(r resources.CreateGameResponse) error {
 	return validation.Errors{
 		"/data/attributes/new_competition":       validation.Validate(r.Data.Attributes.NameCompetition, validation.Required),
-		"/data/attributes/team1":       validation.Validate(r.Data.Attributes.Team1, validation.Length(5,5)),
-		"/data/attributes/team2":        validation.Validate(r.Data.Attributes.Team2, validation.Length(5,5)),
 		"/data/attributes/price":         validation.Validate(r.Data.Attributes.Price, validation.Required),
 	}.Filter()
 }
