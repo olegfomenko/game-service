bbcfe00128b7c385ad6da8ad4ed934f954157827
olegfomenko <oleg.fomenko2002@gmail.com>
2021-03-27 22:05:53 +0200

    fixing create game logic

--- internal/service/handlers/create_game.go
+++ internal/service/handlers/create_game.go
@@ -7,9 +7,12 @@ import (
 	"gitlab.com/distributed_lab/ape/problems"
 	"gitlab.com/tokend/go/xdrbuild"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
+// TODO change constant
 const IssuanceAmount = 1000000
 
 func CreateGame(w http.ResponseWriter, r *http.Request) {
@@ -20,27 +23,37 @@ func CreateGame(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO check error
 	details, err := json.Marshal(map[string]interface{}{
 		"date":  request.Data.Attributes.Date,
 		"team1": request.Data.Attributes.Team1,
 		"team2": request.Data.Attributes.Team2,
 	})
+	if err != nil {
+		Log(r).WithError(err).Error("marshaling details to JSON")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
 
 	date := strings.ReplaceAll(request.Data.Attributes.Date, "-", "")
 	date = strings.ReplaceAll(date, " ", "")
 	date = strings.ReplaceAll(date, ":", "")
 
 	assetCode := "GAM" + date
+	amount := uint64(*request.Data.Attributes.Price)
 
 	assetType, err := Connector(r).GetUint32KeyValue("asset_type:gam")
+	if err != nil {
+		Log(r).WithError(err).Error("error getting key value for asset_type:gam")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
 
 	// TODO check TrailingDigitsCount
 	createGam := &xdrbuild.CreateAsset{
 		RequestID:                0,
 		Code:                     assetCode,
 		MaxIssuanceAmount:        IssuanceAmount,
-		PreIssuanceSigner:        Connector(r).Signer().Seed(),
+		PreIssuanceSigner:        Connector(r).Signer().Address(),
 		InitialPreIssuanceAmount: IssuanceAmount,
 		TrailingDigitsCount:      6,
 		Policies:                 0,
@@ -49,38 +62,31 @@ func CreateGame(w http.ResponseWriter, r *http.Request) {
 		AllTasks:                 nil,
 	}
 
-	// TODO issuer game asset PlayerGam, TeamGam
-
-	// TODO catch error
-	gamBalance, err := Connector(r).Balance(Connector(r).Signer().Address(), assetCode)
-
-	// TODo fix reference
-	paymentToOrg, _ := xdrbuild.CreatePaymentForAccount(xdrbuild.CreatePaymentForAccountOpts{
-		SourceAccountID:      &gamBalance.Relationships.Owner.Data.ID,
-		SourceBalanceID:      gamBalance.ID,
-		DestinationAccountID: "",
-		Amount:               uint64(*request.Data.Attributes.Price),
-		Subject:              "Creating game organizer payment",
-		Reference:            "creating_game_pay_admin" + assetCode,
-		Fee: xdrbuild.Fee{
-			SourceFixed:        0,
-			SourcePercent:      0,
-			DestinationFixed:   0,
-			DestinationPercent: 0,
-			SourcePaysForDest:  false,
-		},
-	})
+	// TODO fix  reference
+	issueGam := &xdrbuild.CreateIssuanceRequest{
+		Reference: strconv.Itoa(time.Now().Nanosecond()),
+		Receiver:  request.Data.Attributes.OwnerId,
+		Asset:     assetCode,
+		Amount:    amount,
+		Details:   json.RawMessage(details),
+		AllTasks:  nil,
+	}
 
 	orgBalance, err := Connector(r).GetBalanceByID(request.Data.Attributes.SourceBalanceId)
+	if err != nil {
+		Log(r).WithError(err).Error("error getting organizer balance")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
 
 	// TODo fix reference
 	paymentToAdmin, _ := xdrbuild.CreatePaymentForAccount(xdrbuild.CreatePaymentForAccountOpts{
 		SourceAccountID:      &orgBalance.Relationships.Owner.Data.ID,
 		SourceBalanceID:      orgBalance.ID,
-		DestinationAccountID: gamBalance.Relationships.Owner.Data.ID,
-		Amount:               uint64(*request.Data.Attributes.Price),
+		DestinationAccountID: Connector(r).Signer().Address(),
+		Amount:               amount,
 		Subject:              "Creating game organizer payment",
-		Reference:            "creating_game_pay_org" + assetCode,
+		Reference:            strconv.Itoa(time.Now().Nanosecond()),
 		Fee: xdrbuild.Fee{
 			SourceFixed:        0,
 			SourcePercent:      0,
@@ -90,6 +96,12 @@ func CreateGame(w http.ResponseWriter, r *http.Request) {
 		},
 	})
 
-	// TODO catch error
-	Connector(r).TxSigned(Connector(r).Signer(), createGam, paymentToOrg, paymentToAdmin)
+	_, err = Connector(r).TxSigned(Connector(r).Signer(), createGam, issueGam, paymentToAdmin)
+	if err != nil {
+		Log(r).WithError(err).Error("error sending transaction")
+		ape.RenderErr(w, problems.BadRequest(err)...)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
