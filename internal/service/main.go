52a59188aeb3524c9d38649b46b4e0ac8d1ce4a3
olegfomenko <oleg.fomenko2002@gmail.com>
2021-03-27 22:10:38 +0200

    add routing

--- internal/service/main.go
+++ internal/service/main.go
@@ -1,44 +1,44 @@
 package service
 
 import (
-    "net"
-    "net/http"
+	"github.com/olegfomenko/game-service/internal/horizon"
+	"net"
+	"net/http"
 
-    "github.com/olegfomenko/game-service/internal/config"
-    "gitlab.com/distributed_lab/kit/copus/types"
-    "gitlab.com/distributed_lab/logan/v3"
-    "gitlab.com/distributed_lab/logan/v3/errors"
+	"github.com/olegfomenko/game-service/internal/config"
+	"gitlab.com/distributed_lab/kit/copus/types"
+	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
 type service struct {
-    log      *logan.Entry
-    copus    types.Copus
-    listener net.Listener
+	log       *logan.Entry
+	copus     types.Copus
+	listener  net.Listener
+	connector *horizon.Connector
 }
 
 func (s *service) run() error {
-    // TODO implement custom logic here
-    r := s.router()
+	r := s.router()
 
-    if err := s.copus.RegisterChi(r); err != nil {
-        return errors.Wrap(err, "cop failed")
-    }
+	if err := s.copus.RegisterChi(r); err != nil {
+		return errors.Wrap(err, "cop failed")
+	}
 
-
-    return http.Serve(s.listener, r)
-    return nil
+	return http.Serve(s.listener, r)
 }
 
 func newService(cfg config.Config) *service {
-    return &service{
-        log:        cfg.Log(),
-        copus:      cfg.Copus(),
-        listener:   cfg.Listener(),
-    }
+	return &service{
+		log:       cfg.Log(),
+		copus:     cfg.Copus(),
+		listener:  cfg.Listener(),
+		connector: cfg.Connector(),
+	}
 }
 
 func Run(cfg config.Config) {
-    if err := newService(cfg).run(); err != nil {
-        panic(err)
-    }
+	if err := newService(cfg).run(); err != nil {
+		panic(err)
+	}
 }
--- internal/service/router.go
+++ internal/service/router.go
@@ -1,23 +1,26 @@
 package service
 
 import (
-    "github.com/go-chi/chi"
-    "github.com/olegfomenko/game-service/internal/service/handlers"
-    "gitlab.com/distributed_lab/ape"
+	"github.com/go-chi/chi"
+	"github.com/olegfomenko/game-service/internal/service/handlers"
+	"gitlab.com/distributed_lab/ape"
 )
 
 func (s *service) router() chi.Router {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
-    r.Use(
-        ape.RecoverMiddleware(s.log),
-        ape.LoganMiddleware(s.log), // this line may cause compilation error but in general case `dep ensure -v` will fix it
-        ape.CtxMiddleware(
-            handlers.CtxLog(s.log),
-        ),
-    )
+	r.Use(
+		ape.RecoverMiddleware(s.log),
+		ape.LoganMiddleware(s.log), // this line may cause compilation error but in general case `dep ensure -v` will fix it
+		ape.CtxMiddleware(
+			handlers.CtxLog(s.log),
+			handlers.CtxConnector(s.connector),
+		),
+	)
 
-    // configure endpoints here
+	r.Route("/integrations/game-service", func(r chi.Router) {
+		r.Post("/create_game", handlers.CreateGame)
+	})
 
-    return r
+	return r
 }
